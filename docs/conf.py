47c8a06dacb6e940a89eed0f78684d8e3e1bfb6e
sarasommariva <sommariva@dima.unige.it>
2022-10-07 16:44:34 +0200

    Create gallery

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 from visbrain.config import CONFIG
@@ -31,9 +31,7 @@ release = '0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    	      'sphinx.ext.autosummary',
-              'sphinx_gallery.gen_gallery']
+extensions = ['sphinx_gallery.gen_gallery']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,10 +54,10 @@ html_theme_options = {
     'navbar_pagenav': False,
     'globaltoc_includehidden': False,
     'navbar_links': [
-        ("API", "api"),
+#        ("API", "api"),
 	("Examples", "auto_examples/index"),
 #	("Reproducing paper results", "auto_paper/index"),
-#        ("GitHub", "https://github.com/elisabettavallarino/transfreq", True)
+        ("GitHub", "https://github.com/sarasommariva/regconnectivity", True)
     ],
     'bootswatch_theme': "cerulean"
 }
@@ -69,14 +67,14 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-examples_dirs = ['../examples']
-gallery_dirs = ['auto_examples']
+examples_dirs = ['../examples', '../examples_paper']
+gallery_dirs = ['auto_examples', 'auto_paper', ]
 
 sphinx_gallery_conf = {
      'examples_dirs': examples_dirs,   # path to your example scripts
      'gallery_dirs': gallery_dirs,  # path to where to save gallery generated output
      'image_scrapers': ('matplotlib'), # figures extension to be embedded
-     #'default_thumb_file': os.path.join('_static', 'ex_tf.png'),
+     'default_thumb_file': os.path.join('_static', 'smiles.jpg'),
      #'within_subsection_order': FileNameSortKey
 }
 
